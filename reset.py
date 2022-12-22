1e1820dbf1a8e47e510a8451fc23e218f9a3361c
Trenton Bauer <trenton.bauer@gmail.com>
2022-12-22 11:21:06 -0600

    updated reset per new table layout

--- reset.py
+++ reset.py
@@ -3,7 +3,6 @@ import os
 os.remove('nick_usage.csv')
 
 file = open('nick_usage.csv', 'w')
-file.write('date,pull_time,wday,location,update_time,pct_full,time\n')
+file.write('pull_date,pull_time,location,update_time,pct_full,date,time,wday\n')
 
 file.close()
-
