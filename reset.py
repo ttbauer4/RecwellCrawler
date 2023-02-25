44148aa0dcb4e74eb34bb83cb6b975bd05e9ba95
Trenton Bauer <trenton.bauer@gmail.com>
2023-02-25 17:54:03 -0600

    made safe for nonexistent output file

--- reset.py
+++ reset.py
@@ -1,6 +1,7 @@
 import os
 
-os.remove('nick_usage.csv')
+if os.path.exists('nick_usage.csv'):
+    os.remove('nick_usage.csv')
 
 file = open('nick_usage.csv', 'w')
 file.write('pull_date,pull_time,location,update_time,pct_full,date,time,wday\n')
